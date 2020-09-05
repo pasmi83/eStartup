851b98541a9c469fa32b40b517f4bd832da26603
pasmi83 <pasmi83@gmail.com>
2020-09-05 09:25:42 +0300

    edited settings

--- estartup/estartup/settings.py
+++ estartup/estartup/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import posixpath
+import psycopg2
+from django.contrib.messages import constants as messages
+from blogitproject.secret_key import EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_PORT, EMAIL_USE_TLS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'estartup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')], #ссылка на папку шаблонов
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,12 +77,14 @@ WSGI_APPLICATION = 'estartup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {"default": {
+        "ENGINE":"django.db.backends.postgresql",
+        "NAME":"blogitdb",
+        "USER":"blogituser",
+        "PASSWORD":"blogituser",
+        "HOST":"localhost",
+        "PORT":"5433",
+        }}
 
 
 # Password validation
@@ -103,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -113,8 +119,33 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATE_INPUT_FORMATS=(
+    '%d.%m.%Y', '%Y-%m-%d', '%d.%m.%y',# '25.10.2006', '25.10.2006', '25.10.06'
+    '%d-%m-%Y', '%d/%m/%Y', '%d/%m/%y',# '25-10-2006', '25/10/2006', '25/10/06'
+    '%d %b %Y',# '25 Oct 2006', 
+    '%d %B %Y',# '25 October 2006', 
+)
+DATETIME_INPUT_FORMATS=(
+    '%d.%m.%y %H:%M', '%d.%m.%y %H:%M:%S','%d.%m.%Y %H:%M', '%d.%m.%Y %H:%M:%S','%Y-%m-%d %H:%M:%S',
+   '%Y-%m-%d %H:%M:%S.%f', '%Y-%m-%d%H:%M',
+  '%Y-%m-%d', '%m/%d/%Y %H:%M:%S', '%m/%d/%Y %H:%M:%S.%f',
+  '%m/%d/%Y %H:%M', '%m/%d/%Y', '%m/%d/%y %H:%M:%S',
+ '%m/%d/%y %H:%M:%S.%f', 
+ '%m/%d/%y %H:%M', '%m/%d/%y')
+DATE_FORMAT = 'd.m.Y'
+DATETIME_FORMAT = 'd.m.Y HH:MM:SS'
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blogitproject/static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+TAGGIT_CASE_INSENSITIVE = True
